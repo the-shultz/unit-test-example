92e6eada7bd6724f36b8a4e89b16c886749fb6a0
the_shultz <aviad202000@yahoo.com>
2021-11-11 17:10:25 +0200

    simple testing introduction (III) attempt to change implementation. Testing to the rescue !

--- src/main/java/mta/industry/skills/testing/unit/calculator/Calculator.java
+++ src/main/java/mta/industry/skills/testing/unit/calculator/Calculator.java
@@ -11,8 +11,28 @@ public class Calculator {
     }
 
     public int times(int x, int y) {
-        return x * y;
+        int result = 0;
+        for (int i=0; i<y; i++) {
+            result += x;
+        }
+        return result;
     }
+
+
+
+
+
+
+
+
+
+    /*public int times(int x, int y) {
+        int result = 0;
+        for (int i=0; i<Math.abs(y); i++) {
+            result += x;
+        }
+        return result;
+    }*/
 
     public int divide(int x, int y) {
         if (y == 0) {
