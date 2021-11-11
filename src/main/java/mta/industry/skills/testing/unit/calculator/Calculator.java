0a5498c2721f6fcc4b7946420ddbc27c524efb23
the_shultz <aviad202000@yahoo.com>
2021-11-11 17:04:36 +0200

    simple testing introduction (II) Enhance code with branches and testing them

--- src/main/java/mta/industry/skills/testing/unit/calculator/Calculator.java
+++ src/main/java/mta/industry/skills/testing/unit/calculator/Calculator.java
@@ -6,5 +6,19 @@ public class Calculator {
         return x + y;
     }
 
-    
+    public int minus(int x, int y) {
+        return x - y;
+    }
+
+    public int times(int x, int y) {
+        return x * y;
+    }
+
+    public int divide(int x, int y) {
+        if (y == 0) {
+            throw new IllegalArgumentException("You cannot divide by 0 !");
+        }
+
+        return x/y;
+    }
 }
--- src/test/java/mta/industry/skills/testing/unit/calculator/CalculatorTest.java
+++ src/test/java/mta/industry/skills/testing/unit/calculator/CalculatorTest.java
@@ -1,19 +1,27 @@
 package mta.industry.skills.testing.unit.calculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class CalculatorTest {
+
+    private Calculator calculatorUnderTest;
+
+    @BeforeEach
+    public void setup() {
+        calculatorUnderTest = new Calculator();
+    }
 
     @Test
     @DisplayName("1 + 1 = 2")
     void addsTwoNumbers() {
-        Calculator calculator = new Calculator();
-        assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
+        assertEquals(2, calculatorUnderTest.add(1, 1), "1 + 1 should equal 2");
     }
 
     @ParameterizedTest(name = "{0} + {1} = {2}")
@@ -24,8 +32,40 @@ public class CalculatorTest {
             "1,  100, 101"
     })
     void add(int first, int second, int expectedResult) {
-        Calculator calculator = new Calculator();
-        assertEquals(expectedResult, calculator.add(first, second),
+        assertEquals(expectedResult, calculatorUnderTest.add(first, second),
                 () -> first + " + " + second + " should equal " + expectedResult);
+    }
+
+    @ParameterizedTest(name = "{0} X {1} = {2}")
+    @CsvSource({
+            "0,    1,   0",
+            "1,    2,   2",
+            "10,  51, 510",
+            "-1,  8, -8",
+            "-2,  -24, 48",
+    })
+    void testTimes(int first, int second, int expectedResult) {
+        assertEquals(expectedResult, calculatorUnderTest.times(first, second),
+                () -> first + " X " + second + " should equal " + expectedResult);
+    }
+
+    @Test
+    @DisplayName("Tests for divide options")
+    void testDivide() {
+
+        // happy path
+        int result = calculatorUnderTest.divide(4, 2);
+        assertEquals(2, result);
+
+        // test the branch
+        try {
+            result = calculatorUnderTest.divide(4, 0);
+            fail("We are expecting to fail with exception on that one, but from some reason didn't !");
+        } catch (IllegalArgumentException e) {
+            // all is ok
+        } catch (Exception e) {
+            fail("We are expecting to fail with IllegalArgumentException on that one, but from some reason we faile with: " + e.getMessage());
+        }
+
     }
 }
